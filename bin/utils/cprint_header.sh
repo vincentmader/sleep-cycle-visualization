39430acf6e712b30bf6b7d5413d73cabc3742abc
Vincent C. Mader <vincent.mader@icloud.com>
2022-11-30 23:04:39 +0100

    minor adjustment to header printing

--- bin/utils/cprint_header.sh
+++ bin/utils/cprint_header.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-clear
-msg="\n"
+msg=""
 msg="$msg ╔═══════════════════════════════════════════╗\n"
 msg="$msg ║ SLEEP-CYCLE DATA ANALYSIS & VISUALIZATION ║\n"
 msg="$msg ╚═══════════════════════════════════════════╝"
 
 color="blue"
 
+clear
 ./cprint.sh "$msg" "$color"
