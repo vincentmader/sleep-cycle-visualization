36c51c19b2f825d0544214291e3fc63fd907822e
Vincent C. Mader <vincent.mader@icloud.com>
2022-12-01 20:10:46 +0100

    reorganized `main.py`

--- src/main.py
+++ src/main.py
@@ -3,22 +3,37 @@ import data_collection
 import data_preparation
 import data_visualization
 
+# DONE 1. save complete sleep history to file
+# DONE 2. load complete sleep history from file
+# TODO 3. visualize sleep history
+# TODO 3.a) plot sleep-snake
+# TODO 3.b) plot "frequency" of sleep notes
+# TODO 3.b) plot histograms of bed-time, wake-up-time, time-in-bed, ...
+# TODO 3...
 
-if __name__ == "__main__":
+
+def collect():
     data_collection.nights.all_nights.get_all_nights()
 
+
+def prepare():
     data_preparation.sleep_cycle.usage.get_usage()
     data_preparation.sleep_notes.names.get_sleepnote_names()
     data_preparation.sleep_notes.names.get_translated_sleepnote_names()
-    data_preparation.sleep_notes.time_series.construct_sleepnote_timeseries_objects()
+    data_preparation.sleep_notes.time_series.construct_sleepnote_timeseries()
 
+
+def process():
+    pass
+
+
+def visualize():
     data_visualization.sleep_cycle.usage.plot_sleep_cycle_usage()
     data_visualization.sleep_notes.moving_average.plot_moving_average()
 
-    # DONE 1. save complete sleep history to file
-    # DONE 2. load complete sleep history from file
-    # TODO 3. visualize sleep history
-    # TODO 3.a) plot sleep-snake
-    # TODO 3.b) plot "frequency" of sleep notes
-    # TODO 3.b) plot histograms of bed-time, wake-up-time, time-in-bed, ...
-    # TODO 3...
+
+if __name__ == "__main__":
+    collect()
+    prepare()
+    process()
+    visualize()
