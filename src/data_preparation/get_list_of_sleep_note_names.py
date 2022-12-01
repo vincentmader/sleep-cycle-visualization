daa917558faed4b20fa76696b020947948bb4a97
Vincent C. Mader <vincent.mader@icloud.com>
2022-12-01 14:21:46 +0100

    removed unused code

--- src/data_preparation/get_list_of_sleep_note_names.py
+++
@@ -1,8 +0,0 @@
-def get_list_of_sleep_note_names(nights):
-    out = []
-    for night in nights:
-        notes = night.sleep_notes
-        for note in notes:
-            if note not in out:
-                out.append(note)
-    return out
